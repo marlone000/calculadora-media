7f4b82530bfb1efcc35b5d750f50f1ba40c9f216
marlon <marlon.spacos@senacsp.edu.br>
2023-09-05 22:07:07 -0300

    calculo de media

---
+++ app.js
@@ -0,0 +1,22 @@
+// comando de saída de dados 
+/*
+alert ("oi, isso é uma mensagem")
+
+console.log("olá, isso é outra mensagem")
+
+alert(10/5)
+
+console.log(10/5)
+*/
+
+nota1 = 4
+nota2 = 2
+nota3 = 2
+nota4 = 8
+
+console.log("a média de João é:")
+console.log((nota1 + nota2 +nota3 + nota4)/4)
+
+media =((nota1 + nota2 + nota3 + nota4)/4)
+
+console.log(media)
